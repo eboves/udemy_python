e27493d7146c21349eab471039574df1ee6ac006
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-11 20:59:17 -0400

    Start working on project. Asking for user input using Turtle

--- Day-025-Project/main.py
+++ Day-025-Project/main.py
@@ -1,1 +1,36 @@
-print("Hellow world")
+import turtle
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. State Game")
+image = "Day-025-Project/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+right_guess = []
+score = 0
+states = 5
+
+answer_state = screen.textinput(title="Guess a State", prompt="What's another State name?").title()
+# print(answer_state)
+data = pd.read_csv("Day-025-Project/50_states.csv")
+# print(data.head)
+coordiantes = data[data['state'] == answer_state]
+# print(coordiantes)
+
+
+
+
+
+
+
+
+# This is how you get the X and Y values used in the CSV file.
+# def get_mouse_click(x, y):
+#     print(x,y)
+
+
+# turtle.onscreenclick(get_mouse_click)
+# turtle.mainloop()screen.exitonclick()
+

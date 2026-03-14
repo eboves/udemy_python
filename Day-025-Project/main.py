c2f63da2ca42e6b6a1bdd05b4aa6aa2324116d49
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-14 17:21:05 -0400

    fix: convert for loop and if statement into a conditional list comprehension

--- Day-025-Project/main.py
+++ Day-025-Project/main.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 screen = turtle.Screen()
 screen.title("U.S. State Game")
-# image = "Day-025-Project/blank_states_img.gif"
-image = "blank_states_img.gif"
+image = "Day-025-Project/blank_states_img.gif"
+# image = "blank_states_img.gif"
 screen.addshape(image)
 turtle.shape(image)
 
@@ -14,8 +14,8 @@ missing_state = []
 score = 0
 states = 5
 
-# data = pd.read_csv("Day-025-Project/50_states.csv")
-data = pd.read_csv("50_states.csv")
+data = pd.read_csv("Day-025-Project/50_states.csv")
+# data = pd.read_csv("50_states.csv")
 # print(data.head)
 
 states = data['state'].to_list()
@@ -25,10 +25,10 @@ while is_game:
     answer_state = screen.textinput(title=f"{score}/50 Guess a State", prompt="What's another State name?").title() 
 
     if answer_state == "Exit" or answer_state == 'E':
-        
-        for state in states:
-            if state not in right_guess:
-                missing_state.append(state)
+        missing_state = [state for state in states if state not in right_guess]
+        # for state in states:
+        #     if state not in right_guess:
+        #         missing_state.append(state)
         new_doc = pd.DataFrame(missing_state)
         new_doc.to_csv("learn_this_states.csv") 
         break
@@ -43,6 +43,7 @@ while is_game:
     print(score)    
     print(right_guess)
     
+    # missing_state = [state for state in states if state not in right_guess]
     
 
 print(right_guess)

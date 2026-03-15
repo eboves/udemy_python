f00a65bf1fe5361bedfd64e29b301a1dc592c585
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-15 06:05:12 -0400

    learned about dictionary comprehension and added data for Day026 Project

--- Day-026/new-day-template/main.py
+++ Day-026/new-day-template/main.py
@@ -11,6 +11,30 @@ Date: [3/13/26]
 Course: Udemy - 100 Days of Code: The Complete Python Pro Bootcamp
 """
 
+""" 02 Pandas Data Frame Dictionary List comprehension """
+# existing_dict = {keys: values}
+# df = pd.DataFrame(existing_dict)
+# new_df = {key: value for (index, row) in df.iterrows()} #this is how to iterate through the rows in a pandas df.
+
+
+
+
+""" 01 Dictionary List comprehension """
+# new_dict = {new_key: new_value for item in dict.items() if test}
+import random
+
+names = ['Elvis', 'Karen', 'no', "ni", 'Jef']
+new_dict = {person:random.randint(1, 100) for person in names}
+# print(new_dict)
+
+above_60 = {n:v for n,v in dict.items(new_dict) if v > 60}
+# print(above_60)
+
+
+
+
+
+""" 00 List comprehension """
 # List Comprehension 
 num = [1,2,3,4]
 # new_list = [new_item for item in list]
@@ -26,13 +50,13 @@ new_range_list = [n * 2 for n in range(1,5)]
 
 names = ['Elvis', 'Karen', 'no', "ni", 'Jef']
 new_name_list = [nam for nam in names if len(nam) < 4]
-print(new_name_list)
+# print(new_name_list)
 # n = "elvis"
 # print(len(n))
 
 num = [2,5,3,7] 
 new_num_list = [n * 3 for n in num if n > 4]
-print(new_num_list)
+# print(new_num_list)
 
 
 

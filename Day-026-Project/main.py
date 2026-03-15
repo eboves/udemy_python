ad861bd9e0e0f3c38f4eb645360f89aa30775193
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-15 19:11:16 -0400

    Finished Day26 project

--- Day-026-Project/main.py
+++ Day-026-Project/main.py
@@ -10,20 +10,47 @@ new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 # print(new_dict)
 
 user_input = input("Pleas enter your name: ").upper()
-# print(user_input)
+print(user_input)
 
 each_user_input_letter = [letter for letter in user_input]
 print(each_user_input_letter)
 
-# new_list = [new_item for item in list if test]
+new_list = [new_item for item in list if test]
 # compare each letter from then user input to see if code of that letter
 phoneric_list = [new_dict[letter] for letter in each_user_input_letter if letter in new_dict] 
 print(phoneric_list)
 
+# 'item_1':{
+#         'places_to_buy': ['Publix', 'Costco', 'BJs', 'Aldi', 'Broward Meat', 'Walmart'],
+#         'is_pack': 0, #should this be a function that ask how many packs 
+#         'need_to_buy': True,
+#         'frequency': 1; #if True add if by weekly
+#     },
+
+# grocery_list = {
+#     'mulos':{
+#         'places_to_buy': ['Costco'],
+#         'is_pack': 0, #should this be a function that ask how many packs 
+#         'need_to_buy': True
+#     },
+#     'eggs':{
+#         'places_to_buy': ['Publix', 'Costco', 'Aldi', 'Walmart'],
+#         'is_pack': 0, #should this be a function that ask how many packs 
+#         'need_to_buy': True
+#     },
+#     'milk':{
+#         'places_to_buy': ['Publix',],
+#         'is_pack': 0, #should this be a function that ask how many packs 
+#         'need_to_buy': False
+#     },
+# }
 
 
 
 
+# grocery_list['Alitas']['places_to_buy'] = ['Costco']
+
+# print(grocery_list)
 
 #TODO 1. Create a dictionary in this format:
 # {"A": "Alfa", "B": "Bravo"}

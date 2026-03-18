205c216122976d5f9e36e0571de911ee1da5c778
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-18 19:50:35 -0400

    feat count down timer working

--- Day-028/main.py
+++ Day-028/main.py
@@ -14,8 +14,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
+def start_timer():
+    count_down(5)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def count_down(count):
+    # print(count)
+    canvas.itemconfig(timer_count,text=count)
+    if count > 0:
+        window.after(1000, count_down, count - 1 )
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -27,21 +34,21 @@ window.config(padx=100, pady=50)
 canvas = tk.Canvas(width=200, height=224)
 tomato_img = tk.PhotoImage(file="Day-028/resources/tomato.png")
 canvas.create_image(103, 112, image=tomato_img)
-canvas.create_text(105, 140, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_count = canvas.create_text(105, 140, text="00:00", fill="white", font=(FONT_NAME, 50, "bold"))
 # canvas.pack()
 canvas.grid(column=1, row=1)
 
-timer_label = tk.Label(text="Timer", fg=GREEN)
+timer_label = tk.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 20, "bold"))
 timer_label.grid(column=1, row=0)
 
-start_btn = tk.Button(text="Start", command="")
+start_btn = tk.Button(text="Start", command=start_timer)
 start_btn.grid(column=0, row=2)
 
 reset_btn = tk.Button(text="Reset", command="")
 reset_btn.grid(column=2, row=2)
 
-complete_check = tk.Checkbutton(text="✔")
-complete_check.grid(column=1, row=3)
+complete_checks = tk.Label(text="✔", fg=GREEN)
+complete_checks.grid(column=1, row=3)
 
 
 

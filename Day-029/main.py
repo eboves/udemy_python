3cf11dc3ded01caa0868700e7f980df36f67ddcd
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-24 20:35:37 -0400

    feat: GUI layout using tkinter grid

--- Day-029/main.py
+++ Day-029/main.py
@@ -1,3 +1,19 @@
+import tkinter as tk
+
+app = tk.Tk()
+app.title("Password Manager")
+app.config(padx=20, pady=20)
+
+canvas = tk.Canvas(width=200, height=200)
+lock_image = tk.PhotoImage(file="Day-029/resources/logo.png")
+canvas.create_image(100, 100, image=lock_image)
+canvas.grid(column=1, row=1)
+
+
+
+app.mainloop()
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #

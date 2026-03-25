b38caa09de5ccc5d6c79c06180508e1641fbe98f
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-24 20:46:40 -0400

    add Labels and Entry fields

--- Day-029/main.py
+++ Day-029/main.py
@@ -7,7 +7,28 @@ app.config(padx=20, pady=20)
 canvas = tk.Canvas(width=200, height=200)
 lock_image = tk.PhotoImage(file="Day-029/resources/logo.png")
 canvas.create_image(100, 100, image=lock_image)
-canvas.grid(column=1, row=1)
+canvas.grid(column=1, row=0)
+
+# ---- WEBSITE LABEL ----
+website = tk.Label(text="Website:")
+website.grid(row=1, column=0)
+
+# ---- WEBSITE ENTRY FIELD ----
+website_entry = tk.Entry(width=35)
+website_entry.grid(row=1, column=1)
+
+# ---- EMAIL/ USER NAME FIELD ----
+email_username = tk.Label(text="Email/Username:")
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -19,3 +40,16 @@ app.mainloop()
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+
+
+
+
+
+
+
+
+
+
+
+

a9d4acb2c780457719795b28cd824a7fbe6d4c4f
Elvis <elvisboves@Elviss-MacBook-Pro.local>
2026-03-26 20:50:54 -0400

    feat: add random gen password

--- Day-029/main.py
+++ Day-029/main.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
+import random
 
 app = tk.Tk()
 app.title("Password Manager")
@@ -10,6 +11,30 @@ lock_image = tk.PhotoImage(file="Day-029/resources/logo.png")
 canvas.create_image(100, 100, image=lock_image)
 canvas.grid(column=1, row=0)
 
+#Password Generator Project
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
+
+list_letters = [random.choice(letters) for letter in range(nr_letters)]
+list_symbols = [random.choice(symbols) for sym in range(nr_symbols)]
+list_numbers = [random.choice(numbers) for num in range(nr_numbers)]
+
+
+password_list = list_letters + list_symbols + list_numbers
+
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
 
 
 def add_pass():

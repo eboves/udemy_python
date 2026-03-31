4d90c903f293ae76c11c6b4bc08d7de66e210c5d
Elvis <e.boves@gmail.com>
2026-03-30 20:07:32 -0400

    refactor: use previous NATO alphabet and add error handler

--- Day-030/main.py
+++ Day-030/main.py
@@ -6,32 +6,31 @@
 
 
 
+import pandas as pd
+
+data = pd.read_csv("Day-026-Project/nato_phonetic_alphabet.csv")
+# print(data.head)
 
 
+new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+# print(new_dict)
+
+def error_handler():
+    user_input = input("Pleas enter your name: ").upper()
+    try: 
+        each_user_input_letter = [new_dict[letter] for letter in user_input]
+    except KeyError as error_message:
+        print(f"enter only letter this {error_message} is not a STRING")
+        error_handler()
+    else:
+        return each_user_input_letter
+
+# new_list = [new_item for item in list if test]
+# compare each letter from then user input to see if code of that letter
+phoneric = error_handler()
+print(phoneric)
 
 
-
-
-
-
-# import pandas as pd
-
-# data = pd.read_csv("Day-026-Project/nato_phonetic_alphabet.csv")
-
-# new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-
-# user_input = " "
-# try:
-#     suser_input = input("Pleas enter your name: ").upper()
-# except:
-#     print("Sorry this is not a word, please enter a word!")
-#     while type(user_input) != " ":
-#         user_input = input("Pleas enter your name: ").upper()
-# else:
-#     each_user_input_letter = [new_dict[letter] for letter in user_input]
-# finally:
-#     print("Hiiiiiii")
-# print(each_user_input_letter)
 
 
 
@@ -42,26 +41,20 @@
 # except:
 #     print("this does not exist")
 
-try:
-    file = open('nothing.txt')
-    lst = ['sdf', 'ghgg']
-    dic = {'key': 'value'}
-    # print(lst[2])
-    print(dic["asdc"])
-except FileNotFoundError:
-    file.open("nothing.txt", 'w')
-    file.write('Something')
-except IndexError:
-    print("This index does not exist")
-except KeyError as error_message:
-    print(f"this key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
+# try:
+#     file = open('some_file.txt')
+#     d = {'key': 'value'}
+#     print(d['key'])
+# except FileNotFoundError:
+#     file = open('some_file.txt', 'w')
+#     file.write("Hello World")
+# except KeyError as error_message:
+#     print(f'This {error_message}, is not a key')
+# else:
+#     content = file.read()
+#     print(content)
 
 
-# new_list = [new_item for item in list if test]
-# compare each letter from then user input to see if code of that letter
-# phoneric_list = [new_dict[letter] for letter in each_user_input_letter if letter in new_dict] 
+
 
 

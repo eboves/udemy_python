60e26ce92e2d0fb6f4afdda40317b0f0aa716ce8
Elvis <e.boves@gmail.com>
2026-04-13 19:19:33 -0400

    add register emails to use SMTP

--- Day-032/main.py
+++ Day-032/main.py
@@ -1,1 +1,16 @@
-print("Hello world")
+import smtplib
+import os
+import dotenv
+# Load environment variables
+dotenv.load_dotenv()
+
+# Get email and password
+my_email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
+
+print(my_email)
+
+# connection = smtplib.SMTP("smtp.gmail.com")
+# connection.starttls()
+
+# "smtp.mail.yahoo.com"

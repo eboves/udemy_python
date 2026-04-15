7465af9502bdf884632169684e70c8eddd3fd8e3
Elvis <e.boves@gmail.com>
2026-04-14 20:21:00 -0400

    this is my brute force

--- Day-032/main.py
+++ Day-032/main.py
@@ -1,12 +1,9 @@
-# import smtplib
-# import os
-# import dotenv
-# # Load environment variables
-# dotenv.load_dotenv()
-
-# # Get email and password
-# my_email = os.getenv("EMAIL")
-# password = os.getenv("PASSWORD")
+import smtplib
+import os
+import dotenv
+import datetime as dt
+import pandas as pd
+import random
 
 # # "smtp.mail.yahoo.com"
 # print("Connecting to Gmail...")
@@ -19,10 +16,51 @@
 #         msg="Subject:Hello\n\n klk manin como tu ta"
 #         )
 
+# import datetime as dt
 
-import datetime as dt
+# now = dt.datetime.now()
+# today_is = now.weekday()
+# year = now.year()
+# print(today_is)
+
+
+# Load environment variables
+dotenv.load_dotenv()
+
+# Get email and password
+my_email = os.getenv("EMAIL")
+password = os.getenv("PASSWORD")
 
 now = dt.datetime.now()
-today_is = now.weekday()
-year = now.year()
-print(today_is)
+today = now.weekday()
+# print(today)
+
+df = pd.read_csv("/Users/elvisboves/Desktop/projects/python/Angela_Yu/udemy_python/Day-032/quotes.txt", sep=",")
+data = df.to_dict()
+
+
+quotes = list(data.values())
+dict_quotes = quotes[0]
+# print(type(dict_quotes))
+quotes = []
+for key, value in dict_quotes.items():
+    quotes.append(value)
+
+quote = random.choice(quotes)
+
+if today == 1:
+    print("Connecting to Gmail...")
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, to_addrs="marcolino_perez@yahoo.com",
+            msg=f"Subject:Quote\n\n {quote}"
+            )
+
+
+
+
+
+
+

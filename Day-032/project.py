3808c19293981ea674789b0bb5b7878581fb8df7
Elvis <e.boves@gmail.com>
2026-04-15 20:32:43 -0400

    reading data from CSV working, now start working on randomly select a letter

--- Day-032/project.py
+++ Day-032/project.py
@@ -9,19 +9,29 @@ dotenv.load_dotenv()
 email = os.getenv("EMAIL")
 password = os.getenv("PASSWORD")
 
-
-
-
 ##################### Extra Hard Starting Project ######################
 
 # 1. Update the birthdays.csv
-data = pd.read_csv("Day-032/birthdays.csv")
+
+# reading data from csv file
+df = pd.read_csv("Day-032/birthdays.csv", index_col=False)
+birthday_month = list(df["month"])
+birthday_day = list(df["day"])
+birthday_name = list(df["name"])
+email = list(df["email"])
+
+
 
 
 # 2. Check if today matches a birthday in the birthdays.csv
 now = dt.datetime.now()
-today = now.month
-print(today)
+month = now.month
+day = now.day
+
+if (month in birthday_month) and (day in birthday_day):
+    index_month = birthday_month.index(month)
+    birthday_person = birthday_name[index_month]
+
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 

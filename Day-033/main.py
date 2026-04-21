4248d999bf1a264957a13c06f9783e189170b16f
Elvis <e.boves@gmail.com>
2026-04-21 18:12:30 -0400

    Getting the position of the ISS using requests

--- Day-033/main.py
+++ Day-033/main.py
@@ -8,6 +8,11 @@ Working with API, using the International Space Station API
 import requests
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(response)
+response.raise_for_status()
 
-print(response)
+data = response.json()
+lon = data["iss_position"]['longitude']
+lat = data["iss_position"]['latitude']
+position = (lon,lat)
+print(position)
+

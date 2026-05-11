638a9d040f2750eaffdcebd120f738637f0d42e9
Elvis <e.boves@gmail.com>
2026-05-11 19:59:24 -0400

    feat: fetching data from openweather

--- Day-035/main.py
+++ Day-035/main.py
@@ -5,12 +5,20 @@ import os
 
 dotenv.load_dotenv()
 
-URL = "https://api.openweathermap.org/data/2.5/forecast?"
+URL = "https://api.openweathermap.org/data/2.5/forecast"
+
 lat = os.getenv("LAT")
 lon = os.getenv("LONG")
 api_key = os.getenv("API_KEY")
+params = {
+    "lat": lat,
+    "lon": lon,
+    "appid": api_key,
+    'cnt': 4
+}
 
-response = requests.get(f"{URL}lat={lat}&lon={lon}&appid={API_KEY}")
+
+response = requests.get(URL, params=params)
 # response = requests.get(URL)
 response.raise_for_status()
 data = response.json()
@@ -22,8 +30,12 @@ weather_description = first_id[0]['description']
 
 print(f"ID: {weather_id}, Description:{weather_description}")
 
+forecast = {}
 for i in list_days:
-    
-    print(i['weather'])
+    id_weather = i['weather'][0]['id']
+    description_weather = i['weather'][0]['description']
+    print(f"ID: {id_weather}, Description: {description_weather}")
+
+
 
 

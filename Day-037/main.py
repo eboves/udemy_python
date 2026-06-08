9f529e81704133b01f2d46b6ae8608384cc13441
Elvis <e.boves@gmail.com>
2026-06-08 19:06:17 -0400

    feat: use post request and put request using pixela

--- Day-037/main.py
+++ Day-037/main.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import dotenv
+from datetime import datetime
 
 dotenv.load_dotenv()
 
@@ -8,6 +9,7 @@ token = os.getenv("TOKEN")
 username = os.getenv("USERNAME")
 
 PIXELA_END_POINT = "https://pixe.la/v1/users"
+GRAPH_ID = 'graph1'
 user_params = {
     'token': token,
     'username': username,
@@ -22,7 +24,7 @@ user_params = {
 # print(response.text)
 
 graph_config = {
-    'id': 'graph1',
+    'id': GRAPH_ID,
     'name': 'coding',
     'unit': 'km',
     'type': 'float',
@@ -33,6 +35,34 @@ headers = {
 }
 graph_end_point = f"{PIXELA_END_POINT}/{username}/graphs"
 
-response = requests.post(url=graph_end_point, json=graph_config, headers=headers)
+# response = requests.post(url=graph_end_point, json=graph_config, headers=headers)
+# print(response.text)
+
+# /v1/users/<username>/graphs/<graphID> 
+
+# GETTING TODAYS DATE SO I DONT HAVE TO TYPE IT MANUALLY
+today = datetime.now()
+today_date = today.strftime("%Y%m%d")
+
+
+# UPDATE DOT GRAPH
+dots_config = {
+    'date':today_date,
+    'quantity': '1.5'
+}
+
+dots_end_point = f"{PIXELA_END_POINT}/{username}/graphs/{GRAPH_ID}"
+# response = requests.post(url=dots_end_point, json=dots_config, headers=headers)
+# print(response.text)
+
+
+# /v1/users/<username>/graphs/<graphID>/<yyyyMMdd>
+
+put_config = {
+    'quantity': '5.6'
+}
+put_end_point = f"{PIXELA_END_POINT}/{username}/graphs/{GRAPH_ID}/{today_date}"
+response = requests.put(url=put_end_point, json=put_config, headers=headers)
 print(response.text)
+
 

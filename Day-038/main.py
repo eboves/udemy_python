66b4fd872168235cfddd1bb453b103959a0415dd
Elvis <e.boves@gmail.com>
2026-06-19 20:55:53 -0400

    feat: fetching data from nutrition API

--- Day-038/main.py
+++ Day-038/main.py
@@ -1,14 +1,21 @@
 import requests
 import dotenv 
 import os
+import datetime
 
 dotenv.load_dotenv()
-x_app_id = os.getenv("XAPPID")
-x_app_key = os.getenv("XAPPKEY")
+
 
 BASED_URL_NUTRITION = "https://app.100daysofpython.dev/v1/nutrition/natural/exercise"
 # BASED_URL = "https://app.100daysofpython.dev/healthz"
+BASED_URL_SHEETS = "https://api.sheety.co/89ec0ccd5c5132abb47011e3c56c278d/ebWorkouts/workouts"
+
+x_app_id = os.getenv("XAPPID")
+x_app_key = os.getenv("XAPPKEY")
+
+
 exercise = input("What excercises you did? ")
+
 headers = {
     "x-app-id": x_app_id,
     "x-app-key": x_app_key,
@@ -18,20 +25,31 @@ data = {
   "query": exercise,
 }
 
-BASED_URL_SHEETS = "https://api.sheety.co/89ec0ccd5c5132abb47011e3c56c278d/ebWorkouts/workouts"
+date = datetime.datetime.now()
+today_date = date.strftime("%x")
+today_time = date.strftime("%X")
+
+# print(today_date, today_time)
+
+response_nutrition = requests.post(BASED_URL_NUTRITION, headers=headers, json=data)
+result_nutrition = response_nutrition.json()
+nutrition_info = result_nutrition["exercises"][0]
+
+duration = nutrition_info['duration_min']
+calories = nutrition_info['nf_calories']
+activity = nutrition_info['name']
+print(activity, duration, calories)
 
 
 
-# print(exercise)
 
-# response = requests.get(BASED_URL)
-# result = response.json()
-# response = requests.post(BASED_URL_NUTRITION, headers=headers, json=data)
-# result = response.json()
-response = requests.get(BASED_URL_SHEETS)
-result = response.json()
 
-print(result)
+
+response_sheets = requests.get(BASED_URL_SHEETS)
+result_sheets = response_sheets.json()
+
+# print(result_nutrition)
+# print(result_sheets)
 
 
 
